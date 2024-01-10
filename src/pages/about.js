f84ebe1ebfeca8131bba2cd166c4740db5e83f48
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-10 14:27:28 +0530

    Component Level Styles Hands-on

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
+import styles from "../styles/About.module.css";
+
 function About() {
   return (
     <>
       <h2>About Page</h2>
       <button className="btn btn-info">Primary</button>
+      <p className={styles.highlight}>About page</p>
     </>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,12 @@
+import styles from "../styles/Contact.module.css";
+
 function Contact() {
-  return <h2>Contact Page</h2>;
+  return (
+    <>
+      <h2>Contact Page</h2>
+      <p className={styles.highlight}>Contact page</p>
+    </>
+  );
 }
 
 export default Contact;
